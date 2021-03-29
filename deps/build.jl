f2e789b0109381e13c1d11929c697fb273c5e5f3
Carlo Baldassi <carlobaldassi@gmail.com>
2021-03-29 18:37:39 +0200

    Remove build.jl

--- deps/build.jl
+++
@@ -1,6 +0,0 @@
-include(joinpath(dirname(@__FILE__), "..", "src", "AtanhErf.jl"))
-
-if !success(`which lockfile`)
-    warn("could not find the `lockfile` program. Try installing it (it's included with `procmail` – www.procmail.org).")
-end
-
